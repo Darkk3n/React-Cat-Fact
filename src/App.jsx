0facb9d780b5f87c69448561880489ab239a5da8
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-05-08 22:17:50 -0600

    Rendered random fact

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+	const [fact, setFact] = useState(null);
+
+	const getRandomFact = () => {
+		fetch("https://catfact.ninja/fact")
+			.then((res) => res.json())
+			.then((data) => setFact(data.fact));
+	};
+
+	useEffect(getRandomFact, []);
+
 	return (
 		<main>
 			<h1>Random Cat Fact App</h1>
+			<h2>{fact}</h2>
 		</main>
 	);
 }
